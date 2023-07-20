8b76fb7fce86a7c54d80ba25be9e2225c9156e97
Umar Baymran <1000slow@gmail.com>
2023-07-20 15:40:11 +0300

    feat(app): Angular Material added

--- apps/news-app/src/app/app.config.ts
+++ apps/news-app/src/app/app.config.ts
@@ -15,6 +15,7 @@ import { provideStore, provideState } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import {NEWS_FEATURE_KEY, newsEffects, NewsFacade, newsReducer} from '@news/data-access';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,21 +23,22 @@ export const appConfig: ApplicationConfig = {
     // provideState(fromNews.NEWS_FEATURE_KEY, fromNews.newsReducer),
     NewsFacade,
     provideStore({
-      [NEWS_FEATURE_KEY]: newsReducer
+        [NEWS_FEATURE_KEY]: newsReducer
     }),
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideHttpClient(),
     importProvidersFrom(NewsFacade),
     {
-      provide: API_URL,
-      useValue: environment.api_url,
+        provide: API_URL,
+        useValue: environment.api_url,
     },
     provideStoreDevtools({
-      maxAge: 25, // Retains last 25 states
-      logOnly: !isDevMode(), // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
-      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+        maxAge: 25,
+        logOnly: !isDevMode(),
+        autoPause: true,
+        trace: false,
+        traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
     }),
-  ],
+    provideAnimations()
+],
 };
